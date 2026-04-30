9493748d42c701992755f8977aa90bca7fccdd6f
Prajwal1905 <prajwalkhade19@gmail.com>
2026-05-01 00:38:48 +0530

    feat: add AppConfig with configurable baseUrl for different environments

---
+++ frontend_mobile/wheat_disease_clean/lib/config.dart
@@ -0,0 +1,10 @@
+class AppConfig {
+  // Change this ONE line for different environments
+  static const String baseUrl = "http://172.31.107.35:8000";
+  
+  // For local testing on laptop:
+  // static const String baseUrl = "http://127.0.0.1:8000";
+  
+  // For production server:
+  // static const String baseUrl = "http://YOUR_SERVER_IP:8000";
+}
