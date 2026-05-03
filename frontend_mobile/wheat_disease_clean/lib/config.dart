dd2a3506299d3b508577b4c68017f9fdb75b0709
Prajwal1905 <prajwalkhade19@gmail.com>
2026-05-03 23:16:23 +0530

    fix: update baseUrl IP address in config.dart

--- frontend_mobile/wheat_disease_clean/lib/config.dart
+++ frontend_mobile/wheat_disease_clean/lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // Change this ONE line for different environments
-  static const String baseUrl = "http://172.31.107.35:8000";
+  static const String baseUrl = "http://10.93.201.35:8000";
   
   // For local testing on laptop:
   // static const String baseUrl = "http://127.0.0.1:8000";
